61e5efaf5d9611d7ae4db0a03eff39da0d3dc964
Angry Segin <lukasz-segin@o2.pl>
2020-02-29 16:53:24 +0100

    second commit

--- src/components/TodoAppComponent.vue
+++ src/components/TodoAppComponent.vue
@@ -1,13 +1,53 @@
 <template>
-  $END$
+  <div id="app">
+    <div class="item">
+      <p>Nowe todo: {{ newItem }}</p>
+      <input type="text" placeholder="todo" v-model="newItem">
+      <button @click="addItem">Dodaj</button>
+    </div>
+    <TodoItem v-on:removeClicked="removeItem" v-bind:item="item" v-for="item in items" v-bind:key="item.id" />
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "TodoAppComponent"
+import TodoItem from "./TodoItem";
+export default {
+  name: 'TodoAppComponent',
+  data() {
+    return {
+      newItem: '',
+      items: [
+        {title: 'Zrobić zakupy', completed: true, id: 1},
+        {title: 'Nagrać kurs', completed: false, id: 2}
+      ]
+    }
+  },
+  components: {
+    TodoItem
+  },
+  methods: {
+    addItem() {
+      this.items.push({ title: this.newItem, completed: false, id: Math.random()});
+      this.newItem = '';
+    },
+    removeItem(id) {
+      const index = this.items.findIndex(el => el.id === id);
+      this.items[index].completed = true;
+    }
   }
+}
 </script>
 
 <style scoped>
-
+  .item {
+    border: 1px solid chocolate;
+    padding: 5px;
+    margin: 5px;
+  }
+  .completed {
+    opacity: 0.5;
+  }
+  .completed h2 {
+    text-decoration: line-through;
+  }
 </style>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,13 +1,31 @@
 <template>
-  $END$
+    <div class="item" v-bind:class="{ completed: item.completed}">
+      <h2>{{ item.title }}</h2>
+      <button v-if="!item.completed" @click="removeItem">Zrobione</button>
+    </div>
 </template>
 
 <script>
   export default {
-    name: "TodoItem"
+    name: "TodoItem",
+    props: ['item'],
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      removeItem() {
+        this.$emit('removeClicked', this.item.id)
+      }
+    }
   }
 </script>
 
 <style scoped>
-
+  .item {
+    border: 1px solid chocolate;
+    padding: 5px;
+    margin: 5px;
+  }
 </style>
